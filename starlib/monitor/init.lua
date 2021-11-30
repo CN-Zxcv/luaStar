f6a8a0ee2e923ae8f1adf02f654cfd2c7561c1b2
CN-Zxcv <hx@CDA.localdomain>
2021-11-30 21:46:27 +0800

    mod: monitor

--- starlib/monitor/init.lua
+++ starlib/monitor/init.lua
@@ -14,18 +14,47 @@ local function run(name, num, fn)
     return string.format('monitor=%s, num=%s, use=%s', name, num, use())
 end
 
+--[[
+fns = {
+    what = func()
+    end,
+}
+-- ]]
 local function compare(name, num, fns)
-    local result = {}
+    local raw = {}
+
     local min = math.huge
-    for name, fn in pairs(fns) do
-        local watch = clock()
-        for i = 1, num do
-            fn()
+    collectgarbage('stop')
+    local loop = 10
+    for i = 1, loop do
+        for name, fn in pairs(fns) do
+            collectgarbage()
+            local watch = clock()
+            for i = 1, num do
+                fn()
+            end
+            local use = watch()
+            local t = raw[name]
+            if not t then
+                t = {}
+                raw[name] = t
+            end
+            table.insert(t, use)
         end
-        local use = watch()
-        table.insert(result, {name, use})
-        min = math.min(min, use)
-        -- table.insert(report, string.format('    name=%s, use=%s', v[1], use()))
+    end
+    collectgarbage('restart')
+
+    local result = {}
+    local cut = loop * 0.2
+    for k, v in pairs(raw) do
+        table.sort(v)
+        local sum = 0
+        for i = cut, loop - cut do
+            sum = sum + v[i]
+        end
+        local n = sum / (loop - cut * 2)
+        table.insert(result, {k, n})
+        min = math.min(min, n)
     end
 
     table.sort(result, function(a, b) return a[1] < b[1] end)
