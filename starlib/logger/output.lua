244f529388a85dc1f0f06616ff229d0b525ddb8d
CN-Zxcv <hx@CDA.localdomain>
2021-12-11 18:48:53 +0800

    mod: log output format

--- starlib/logger/output.lua
+++ starlib/logger/output.lua
@@ -16,7 +16,7 @@ local Prefix = logger.LogPrefix
 local Color = {36,32,33,31,35}
 
 local function output(lv, prefix, fmt, ...)
-    local curDate = os.date('%Y%m%d-%H:%M:%S')
+    local curDate = os.date('%Y%m%d %H:%M:%S')
     print(string.format("\27[%dm%s %s %s " .. (fmt or 'nil') .. "\27[0m", Color[lv], Prefix[lv], curDate, prefix, ...))
 end
 
