5fde672a0ea0600d2bc83f7af22032168c2a2955
CN-Zxcv <hx@CDA.localdomain>
2021-12-11 18:47:28 +0800

    mod: debug.dump

--- starlib/extension/debug.lua
+++ starlib/extension/debug.lua
@@ -1,91 +1,72 @@
-local debug = _G.debug
 
-local function mkSpace(num)
-    local fmt = string.format("%%%d.%ds", num, num)
-    return string.format(fmt, " ")
-end
+_ENV = _G.debug
 
-local function rvalueToStr(x)
-    if type(x) == 'string' then
+local tinsert = table.insert
+local tconcat = table.concat
+
+local function rvalue(x)
+    local typename = type(x)
+    if typename == 'string' then
         return string.format('%q', x)
-    else
+    elseif typename == 'number' 
+        or typename == 'boolean' 
+        then
         return tostring(x)
+    else
+        return string.format('(%s)', type(x))
     end
 end
 
-local function lvalueToStr(x)
-    local name = type(x)
-    if name == 'string' then
-        if tonumber(x) then
-            return string.format('[%q]', x)
-        else
-            return x
-        end
-    elseif name == 'number' then
-        return tostring(x)
+local function lvalue(x)
+    local typename = type(x)
+    if typename == 'string' then
+        return x
+    elseif typename == 'number' then
+        return string.format('[%s]', x)
     else
-        return string.format('[(%s:%s)]', name, tostring(x))
+        return string.format('(%s)', x)
     end
 end
 
-local function dump(t, dumped, step, depth)
-    local str = ""
-    step = step or 0
-    if type(t) == "table" then
-        if step == 0 then 
-            str = "{\n"
-        end
-    
-        if depth and depth == 0 then
-            str = str .. string.format("%s...(table)\n", mkSpace(step * 2 + 2))
+local function runDump(ret, t, dumped, depth, max_depth)
+    if type(t) == 'table' then
+        if depth == max_depth then
+            tinsert(ret, rvalue(t))
+        elseif dumped[t] then
+            tinsert(ret, string.format("(<dup>%s)", tostring(t)))
         else
+            dumped[t] = true
+
+            tinsert(ret, '{')
+            tinsert(ret, string.rep(' ', depth * 2))
+            tinsert(ret, '\n')
+            local space = string.rep(' ', (depth + 1) * 2)
             for k, v in pairs(t) do
-                if type(v) == "table" then
-                    local mt = getmetatable(v)
-                    if mt and mt.__tostring then
-                        str = str .. string.format('%s%s = %s\n', mkSpace(step * 2 + 2), lvalueToStr(k), tostring(v))
-                    elseif dumped[v] then
-                        str = str .. string.format('%s%s = <%s>\n', mkSpace(step * 2 + 2), lvalueToStr(k), v)
-                    else
-                        dumped[t] = true
-                        str = str..string.format("%s%s = {\n", mkSpace(step*2+2), lvalueToStr(k))
-                        str = str..dump(v, dumped, step+1, depth and depth - 1)
-                        str = str..string.format("%s}\n", mkSpace(step*2+2))
-                    end
-                else
-                    str = str..string.format("%s%s = %s\n", mkSpace(step*2+2), lvalueToStr(k), rvalueToStr(v))
-                end
+                tinsert(ret, space)
+                tinsert(ret, lvalue(k))
+                tinsert(ret, ' = ')
+                runDump(ret, v, dumped, depth + 1, max_depth)
+                tinsert(ret, '\n')
             end
+            tinsert(ret, string.rep(' ', depth * 2))
+            tinsert(ret, '}')
         end
-
-        if step == 0 then
-            if str == "{\n" then 
-                str = "{ " 
-            end
-            
-            str = string.format("%s}", str)
-        end
-
-    elseif type(t) == 'nil' then
-        -- nothing
-        str = str .. 'nil'
     else
-        str = str..string.format("%s%s", mkSpace(step*2), rvalueToStr(t))
+        tinsert(ret, rvalue(t))
     end
-	
-    return str
+    return ret
 end
 
-function debug.dump(t, depth)
-    return dump(t, {}, nil, depth)
+function dump(t, depth)
+    return tconcat(runDump({}, t, {}, 0, depth or 16))
 end
 
-function debug.setenv(fn, env)
+function setenv(fn, env)
     for i = 1, math.huge do
-        local n = debug.getupvalue(fn, i)
+        local n = getupvalue(fn, i)
         if n == '_ENV' then
-            debug.upvaluejoin(fn, i, function() return name end, 1)
-            debug.setupvalue(fn, i, env)
+            upvaluejoin(fn, i, function() return name end, 1)
+            setupvalue(fn, i, env)
         elseif not n then
             break
         end
@@ -93,13 +74,38 @@ function debug.setenv(fn, env)
     return fn
 end
 
-function debug.getenv(fn)
+function getenv(fn)
     for i = 1, math.huge do
-        local n, v = debug.getupvalue(fn, i)
+        local n, v = getupvalue(fn, i)
         if n == '_ENV' then
             return v
         elseif not n then
             break
+        end
+    end
+end
+
+if not getfenv then
+    function getfenv(f)
+        local cf, up = type(f) == 'function' and f or getinfo(f + 1, 'f').func, 0
+        local name, val
+        repeat
+            up = up + 1
+            name, val = getupvalue(cf, up)
+        until name == '_ENV' or name == nil
+        if val then
+            return val
+        end
+
+        if type(f) == 'number' then
+            f, up = f + 1, 0
+            repeat
+                up = up + 1
+                name, val = getlocal(f, up)
+            until name == '_ENV' or name == nil
+            if val then 
+                return val 
+            end
         end
     end
 end
